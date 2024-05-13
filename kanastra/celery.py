9152258acc31f736d53d791e39eb8c646245900c
jotabraga <jpmbraga.feg@gmail.com>
2024-05-13 10:56:31 -0300

    feat: celery config

--- kanastra/celery.py
+++ kanastra/celery.py
@@ -3,7 +3,12 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kanastra.settings")
 app = Celery("kanastra")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
