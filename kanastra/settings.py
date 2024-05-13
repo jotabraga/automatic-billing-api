524fd1c21d1ea5487cd23f5e9d581103954920a6
jotabraga <jpmbraga.feg@gmail.com>
2024-05-13 10:36:53 -0300

    feat: add celery to app

--- kanastra/settings.py
+++ kanastra/settings.py
@@ -65,6 +65,10 @@ DATABASES = {
     }
 }
 
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
