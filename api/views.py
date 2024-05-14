187782f83820d59a0a2f22a70ffdd2baac0a7977
jotabraga <jpmbraga.feg@gmail.com>
2024-05-14 12:34:12 -0300

    feat: order file records by created at date

--- api/views.py
+++ api/views.py
@@ -24,7 +24,7 @@ def apiOverview(request):
 
 @api_view(["GET"])
 def fileList(_request: Request):
-    files = FileUploaded.objects.all()
+    files = FileUploaded.objects.all().order_by("-created_at")
     serializer = FileUploadedSerializer(files, many=True)
     return Response(serializer.data)
 
