37e4fa25127bacfe3b9afc1c7de06527da1db56f
jotabraga <jpmbraga.feg@gmail.com>
2024-05-15 09:37:52 -0300

    feat: add email send to celery tasks

--- api/tasks.py
+++ api/tasks.py
@@ -2,81 +2,87 @@ from celery import shared_task
 from pyboleto.bank.bradesco import BoletoBradesco
 from pyboleto.pdf import BoletoPDF
 import datetime
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
 
 
 @shared_task
 def process_csv_lote(lote):
     for line in lote:
         boleto = generate_boleto(line.decode("utf-8"))
-
-
-def divide_file_into_chunks(file, chunk_size=10000):  # Chunk size de 1MB
-    chunks = []
-    while True:
-        chunk = file.read(chunk_size)
-        if not chunk:
-            break
-        chunks.append(chunk)
-    return chunks
-
-
-def combine_results(results):
-    combined_result = []
-
-    for result in results:
-        combined_result.extend(result)
-
-    return combined_result
+        send_mail(boleto, line)
 
 
 def generate_boleto(row):
-    print(row)
-
-    # name = row["name"]
-    # government_id = row["governmentId"]
-    # email = row["email"]
-    # debt_amount = row["debtAmount"]
-    # debt_due_date = row["debtDueDate"]
-    # debt_id = row["debtID"]
+    government_id = row["governmentId"]
+    debt_amount = row["debtAmount"]
+    debt_due_date = row["debtDueDate"]
+    debt_id = row["debtID"]
     listaDadosBradesco = []
 
     for i in range(2):
-        d = BoletoBradesco()
-        d.carteira = "06"  # dado do bradesco
-        d.cedente = "Empresa ACME LTDA"
-        d.cedente_documento = "102.323.777-01"
-        d.cedente_endereco = (
+        new_boleto = BoletoBradesco()
+        new_boleto.carteira = "06"  # dado do bradesco
+        new_boleto.cedente = "Kanastra"
+        new_boleto.cedente_documento = government_id
+        new_boleto.cedente_endereco = (
             "Rua Acme, 123 - Centro - Sao Paulo/SP - " + "CEP: 12345-678"
         )
-        d.agencia_cedente = "0278-0"
-        d.conta_cedente = "43905-3"
+        new_boleto.agencia_cedente = "0278-0"
+        new_boleto.conta_cedente = "43905-3"
 
-        d.data_vencimento = datetime.date(2011, 1, 25)
-        d.data_documento = datetime.date(2010, 2, 12)
-        d.data_processamento = datetime.date(2010, 2, 12)
+        new_boleto.data_vencimento = datetime.date(debt_due_date)
+        new_boleto.data_documento = datetime.date(2010, 2, 12)
+        new_boleto.data_processamento = datetime.date(2010, 2, 12)
 
-        d.instrucoes = [
+        new_boleto.instrucoes = [
             "- Linha 1",
             "- Sr Caixa, cobrar multa de 2% após o vencimento",
             "- Receber até 10 dias após o vencimento",
         ]
-        d.demonstrativo = [
+        new_boleto.demonstrativo = [
             "- Serviço Teste R$ 5,00",
             "- Total R$ 5,00",
         ]
-        d.valor_documento = 2158.41
+        new_boleto.valor_documento = debt_amount
 
-        d.nosso_numero = "1112011668"
-        d.numero_documento = "1112011668"
-        d.sacado = [
+        new_boleto.nosso_numero = debt_id
+        new_boleto.numero_documento = debt_id
+        new_boleto.sacado = [
             "Cliente Teste %d" % (i + 1),
             "Rua Desconhecida, 00/0000 - Não Sei - Cidade - Cep. 00000-000",
             "",
         ]
-        listaDadosBradesco.append(d)
+        listaDadosBradesco.append(new_boleto)
 
     boleto = BoletoPDF("boleto-bradesco-formato-normal-teste.pdf")
     for i in range(len(listaDadosBradesco)):
         boleto.drawBoleto(listaDadosBradesco[i])
         boleto.nextPage()
-    # boleto.save()
+    boleto.save()
+    return boleto
+
+
+def send_mail(boleto, row):
+    name = row["name"]
+    email = row["email"]
+
+    subject = f"Email de cobrança para {name}"
+    body = "Pague seu boleto e evite cobranças"
+    sendmail = EmailMultiAlternatives(
+        subject, body, settings.DEFAULT_FROM_EMAIL, to=[email]
+    )
+    sendmail.attach("emailpdf_{}".format(name) + ".pdf", boleto, "application/pdf")
+    sendmail.content_subtype = "pdf"
+    sendmail.decode = "utf-8"
+    return sendmail.send()
+
+
+# get results of all tasks, its important to do in the future:
+# def combine_results(results):
+#     combined_result = []
+
+#     for result in results:
+#         combined_result.extend(result)
+
+#     return combined_result
